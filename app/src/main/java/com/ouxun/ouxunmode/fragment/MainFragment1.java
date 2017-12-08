f8c6bfab40e14fe8b9c20b65b17c8d4815d4d9e3
admin <425116228@qq.com>
2017-12-08 16:28:58 +0800

    修改测试

--- app/src/main/java/com/ouxun/ouxunmode/fragment/MainFragment1.java
+++ app/src/main/java/com/ouxun/ouxunmode/fragment/MainFragment1.java
@@ -1,6 +1,17 @@
 package com.ouxun.ouxunmode.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.ouxun.ouxunmode.R;
 import com.ouxun.ouxunmode.base.BaseFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by Administrator on 2017/12/1 0001.
@@ -8,6 +19,10 @@ import com.ouxun.ouxunmode.base.BaseFragment;
  * 功能描述：
  */
 public class MainFragment1 extends BaseFragment {
+    @BindView(R.id.test_hello)
+    TextView testHello;
+    Unbinder unbinder;
+
     @Override
     public int getContentViewId() {
         return R.layout.fragment_main1;
@@ -21,5 +36,19 @@ public class MainFragment1 extends BaseFragment {
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO: inflate a fragment view
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
